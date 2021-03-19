cb6f29e3e9e3c57e807bb0ab227ad8a49b2b2024
Samuel Lindemer <samuel.lindemer@gmail.com>
2021-03-19 15:17:19 +0100

    Configuration register selction not working

--- src/main/scala/Pmp.scala
+++ src/main/scala/Pmp.scala
@@ -55,22 +55,6 @@ import spinal.lib._
  * register defines a 4-byte wide region.
  */
 
-class PmpCfg(preset : UInt = U"32'0") extends Bundle {
-  val encoded = preset
-
-  def cfg = encoded.subdivideIn(8 bits)
-  def r = cfg.map(c => c(0))
-  def w = cfg.map(c => c(1))
-  def x = cfg.map(c => c(2))
-  def a = cfg.map(c => c(4 downto 3))
-  def l = cfg.map(c => c(7))
-  
-  def asMask : Bits = { 
-    val mask = l.map(l => l ? B"8'x00" | B"8'xff")
-    mask(0) ## mask(1) ## mask(2) ## mask(3)
-  }
-}
-
 class Pmp(count : Int) extends Component {
   assert(count % 4 == 0)
   assert(count <= 16)
@@ -81,37 +65,56 @@ class Pmp(count : Int) extends Component {
     val writeData = in UInt(32 bits)
     val readData = out UInt(32 bits)
   }
-  val cfgs = Mem(UInt(32 bits), Array.fill(count / 4)(U"32'0"))
-  val addrs = Mem(UInt(30 bits), count)
 
-  val cfg = cfgs.readAsync(count match {
+  val pmpAddr = Mem(UInt(32 bits), count)
+  val pmpCfg = Reg(Bits(8 * count bits)) init(0)
+
+  // io.index is a 2-bit CSR selector when accessing pmpCfg
+  val csrNum = count match {
     case 4 => U"0"
-    case _ => io.index((log2Up(count) - 1) downto 2)
-  })
-
-  cfgs.write(
-    count match {
-      case 4 => U"0"
-      case _ => io.index((log2Up(count) - 3) downto 0)
-    },
-    io.writeData,
-    io.write && io.select,
-    ~(cfg(31) ## B"b11" ## cfg(31) ## cfg(31) ## cfg(31) ## cfg(31) ## cfg(31) ## 
-      cfg(23) ## B"b11" ## cfg(23) ## cfg(23) ## cfg(23) ## cfg(23) ## cfg(23) ## 
-      cfg(15) ## B"b11" ## cfg(15) ## cfg(15) ## cfg(15) ## cfg(15) ## cfg(15) ## 
-      cfg( 7) ## B"b11" ## cfg( 7) ## cfg( 7) ## cfg( 7) ## cfg( 7) ## cfg( 7))
-  ) 
-  
-  val locked = cfg.subdivideIn(8 bits)(io.index(1 downto 0))(7)
-  addrs.write(
-    io.index,
-    io.writeData(29 downto 0),
-    io.write && ~io.select && ~locked 
-  )
-
-  when(io.select) {
-    io.readData := cfg
-  } otherwise {
-    io.readData := U"2'00" @@ addrs.readSync(io.index)
+    case _ => io.index(log2Up(count) - 3 downto 0)
   }
+  val csr = pmpCfg.subdivideIn(32 bits)(csrNum)
+
+  // io.select === 1 for access to pmpCfg
+  when (io.select) {
+    when (io.write) {
+
+      // get the requested configuration CSR from the bit vector
+      switch(csrNum) {
+        for (i <- 0 until (count / 4)) {
+          is(i) {
+
+            // for each region in the configuration CSR
+            for (j <- Range(0, 32, 8)) {
+
+              // if the region isn't locked, overwrite it with io.writeData
+              when (~pmpCfg(j + 7 + 32 * i)) {
+                val readSet = j + 7 downto j
+                val writeSet = j + 7 + 32 * i downto j + 32 * i
+                pmpCfg(readSet).assignFromBits(io.writeData.asBits(readSet))
+              }
+            }
+          }
+        }
+      }
+    }
+    io.readData.assignFromBits(csr)
+
+  // io.select === 0 for access to pmpAddr
+  } otherwise {
+    io.readData := pmpAddr.readAsync(io.index)
+    when (io.write) {
+
+      // check the lock bit on the corresponding configuration register
+      val lock = csr.subdivideIn(8 bits)(io.index(1 downto 0))(7)
+      pmpAddr.write(
+        io.index,
+        io.writeData,
+        io.write && ~io.select && ~lock
+      )
+    }
+  }
+
+
 }
